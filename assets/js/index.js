7cc9f3b4f9e582658f41dc05235bbcce05a5f404
Jaime Wyant <jwyant@sfbcic.com>
2015-05-07 14:13:56 -0500

    * Updated datepicker to respect previously set interpolation begin /   end markers.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,14 @@
 (function (angular) {
   'use strict';
 
-  angular.module('720kb', [
+  var app = angular.module('720kb', [
     'ngRoute',
     '720kb.datepicker'
   ]);
+  
+  // Setup interpolation to use [[ / ]]
+  app.config(function($interpolateProvider) {
+        // Setup angular to use [[ expression ]] instead of {{ expression }}
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+  });
 }(angular));
